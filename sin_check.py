03c963374d963f61e1be287e64ff2b23db0f91d2
= <alexander.mcvittie@gmail.com>
2017-12-17 11:43:30 -0500

    Made it a one liner

--- sin_check.py
+++ sin_check.py
@@ -1,12 +1,5 @@
 #!/usr/bin/env python3
 def checker(sin_number):
-    sin_number = list(str(sin_number))
-    for x in range(1, len(sin_number), 2):
-        sin_number[x] = sum([int(d) for d in list(str(int(sin_number[x]) * 2)) ])
-    return sum( [int(d) for d in sin_number]) % 10 == 0
+    return sum(map(lambda x:sum(map(int,str(int(x[0]) * x[1]))),zip(sin_number, [1, 2] * 5))) % 10 == 0
 
-
-
-print(checker(str(input("Enter your SIN (EX xxxxxxxxx) > "))))
-
-
+print(checker("046454286"))
