6856aaaae07c542e051186b3dc3aedd9e1d6414e
Alex McVittie <mcvittal@users.noreply.github.com>
2018-11-30 11:07:37 -0500

    Add sending image as attachment

--- checkwinner.sh
+++ checkwinner.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
-XDG_RUNTIME_DIR=/run/user/1000
+
 /home/alex/node_modules/pageres-cli/cli.js --delay=10 https://2018.spaceappschallenge.org/awards/global-finalists --filename=img
 
 v=$(md5sum img.png)
 v=$(echo $v | awk '{print $1;}')
-rm img.png
+# rm img.png
 echo $v
 if [ "$v" != "a742879ddf017e87c810e03c6420ef51" ]; then
-	ssh ajmcvitt@caffeine.csclub.uwaterloo.ca 'echo "Check https://2018.spaceappschallenge.org/awards/global-finalists" | mail -s "GLOBAL STANDINGS PAGE CHANGED" alexander.mcvittie@gmail.com'
+	scp img.png ajmcvitt@taurine.csclub.uwaterloo.ca:/users/ajmcvitt/img.png
+	ssh ajmcvitt@taurine.csclub.uwaterloo.ca 'echo "Check https://2018.spaceappschallenge.org/awards/global-finalists" | mail -s "GLOBAL STANDINGS PAGE CHANGED" alexander.mcvittie@gmail.com -A /users/ajmcvitt/img.png'
+	ssh ajmcvitt@taurine.csclub.uwaterloo.ca 'rm /users/ajmcvitt/img.png'
 else
 	echo $(date) >> /home/alex/noresults.txt
 	
 fi
+rm img.png
 
 
